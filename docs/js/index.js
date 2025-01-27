2a5075c3c611d6de9f80ed935a482fa2552bfcee
Elyjun <julien.paulus@oclock.school>
2025-01-27 19:33:37 +0100

    update the style cards and add contact section

--- docs/js/index.js
+++ docs/js/index.js
@@ -10,6 +10,7 @@ function toggleDarkMode() {
     { selector: ".section_tech", className: "tech_light" },
     { selector: ".music_container", className: "tech_light" },
     { selector: ".section_projet", className: "section_light" },
+    { selector: ".section_contact", className: "section_light" },
     { selector: ".footer_navbar", className: "footer_light" }
   ];
 
@@ -36,12 +37,55 @@ const translateToFrench = document.getElementById("translateToFrench");
 function translatePage(toLang) {
   const elements = document.querySelectorAll("[data-fr][data-en]");
   elements.forEach((el) => {
-    el.innerHTML = el.getAttribute(`data-${toLang}`);
+    const translation = el.getAttribute(`data-${toLang}`);
+    if (translation) {
+      // Trouver l'icône (ou tout autre élément enfant)
+      const iconElement = Array.from(el.childNodes).find(
+        (node) => node.nodeType === Node.ELEMENT_NODE
+      );
+
+      // Réinitialiser le contenu (texte uniquement, pas l'icône)
+      el.innerHTML = "";
+      const textNode = document.createTextNode(translation);
+
+      // Réinsérer dans le bon ordre : icône + texte
+
+      el.appendChild(textNode); // Place le texte traduit à côté
+
+      if (iconElement) {
+        el.appendChild(iconElement); // Garde l'icône à gauche
+      }
+    }
   });
 
+  // Basculer les boutons de traduction
   translateToEnglish.style.display = toLang === "fr" ? "inline-block" : "none";
   translateToFrench.style.display = toLang === "en" ? "inline-block" : "none";
+
+  // Vérifier les icônes après le changement de langue
+  resetIcons();
+}
+
+function resetIcons() {
+  // Vérifiez si le mode sombre est activé et ajustez les icônes
+  const isDarkModeActive = document
+    .querySelector(".body_container")
+    .classList.contains("dark-mode");
+  darkModeIcon.style.display = isDarkModeActive ? "none" : "inline-block";
+  lightModeIcon.style.display = isDarkModeActive ? "inline-block" : "none";
 }
 
 translateToEnglish.addEventListener("click", () => translatePage("en"));
 translateToFrench.addEventListener("click", () => translatePage("fr"));
+
+document.querySelectorAll(".flip-button").forEach((button) => {
+  button.addEventListener("click", (event) => {
+    event.stopPropagation();
+    console.log("Bouton cliqué :", button);
+    const cardInner = button.closest(".card-inner");
+    console.log("Element parent trouvé :", cardInner);
+    if (cardInner) {
+      cardInner.classList.toggle("flipped");
+    }
+  });
+});
